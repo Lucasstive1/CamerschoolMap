e7aa6c1411cb3e98ef4cdf2570918507e17314dd
Lucasstive1 <stivelucas037@gmail.com>
2025-04-06 04:51:46 +0200

    erreur resoluts

--- CamerschoolMap/CamerschoolMap/urls.py
+++ CamerschoolMap/CamerschoolMap/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpattern
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('CamerSchool.urls')),   
@@ -13,4 +13,4 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-urlpatterns += staticfiles_urlpattern()
+urlpatterns += staticfiles_urlpatterns()
